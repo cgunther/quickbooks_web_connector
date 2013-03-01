4af4e35980d26b929c40f8bfc02b1a6a13ede9af
Chris Gunther <chris@room118solutions.com>
2013-02-28 21:43:17 -0500

    enable get requests on soap endpoint so QWC can verify the certificate

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ QuickbooksWebConnector::Engine.routes.draw do
   get 'qwc' => 'qwc#download', defaults: { format: :xml }
   post 'soap' => 'soap#endpoint'
 
+  # QWC will perform a GET request to verify the SSL certificate
+  get 'soap' => 'soap#endpoint'
+
 end
