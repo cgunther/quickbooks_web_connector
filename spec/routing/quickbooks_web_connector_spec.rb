fecd8572a110702ac144c443786e21114ebf0037
Chris Gunther <chris@room118solutions.com>
2013-11-21 14:07:50 -0500

    Use new RSpec routes method for defining the routes in a routing spec

--- spec/routing/quickbooks_web_connector_spec.rb
+++ spec/routing/quickbooks_web_connector_spec.rb
@@ -2,9 +2,7 @@ require 'spec_helper'
 
 describe 'QuickbooksWebConnector routing' do
 
-  before do
-    @routes = QuickbooksWebConnector::Engine.routes
-  end
+  routes { QuickbooksWebConnector::Engine.routes }
 
   it 'defaults to XML for downloading QWC' do
     expect(get: '/qwc').to route_to(controller: 'quickbooks_web_connector/qwc', action: 'download', format: :xml)
