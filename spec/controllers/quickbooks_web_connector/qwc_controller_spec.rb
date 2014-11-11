bd4d4b450f60e83a55c61f0daea8051cb8e439c2
Chris Gunther <chris@room118solutions.com>
2014-11-11 17:12:37 -0500

    Combine specs for QwcController into a single example

--- spec/controllers/quickbooks_web_connector/qwc_controller_spec.rb
+++ spec/controllers/quickbooks_web_connector/qwc_controller_spec.rb
@@ -3,26 +3,14 @@ require 'spec_helper'
 RSpec.describe QuickbooksWebConnector::QwcController, type: :controller do
 
   describe 'GET :download' do
-    before { get :download, format: :xml, use_route: 'quickbooks_web_connector' }
+    it 'renders the QWC file' do
+      get :download, format: :xml, use_route: 'quickbooks_web_connector'
 
-    it 'responds with success' do
       expect(response).to be_success
-    end
-
-    it 'responds as XML' do
       expect(response.header['Content-Type']).to match(/application\/xml/)
-    end
-
-    it 'sends the file as an attachment' do
       expect(response.header['Content-Disposition']).to match(/attachment/)
-    end
-
-    it 'names the file' do
       expect(response.header['Content-Disposition']).to match(/qbwc\.qwc/)
-    end
-
-    it 'renders the qwc template' do
-      assert_template :qwc
+      expect(response).to render_template(:qwc)
     end
   end
 
